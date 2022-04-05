b14dfbe3ee239c62b37d7bbeadde41abd2b58db2
iparamosh <91302209+iparamosh@users.noreply.github.com>
2022-04-05 09:51:51 -0700

    wrote hi

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Main {
 	public static void main(String[] args) {
-		
+		System.out.print("hello");
 		  int buttonSize = 0, boardSize = 0; Scanner sc = new Scanner(System.in);
 		  System.out.println("What button size do you want?"); buttonSize =
 		  sc.nextInt(); System.out.println("What board size do you want?"); boardSize =
